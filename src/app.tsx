e9e5f542c08e1c640a95db76a9ec2e704986b18e
nathannfs <nathann.santoss2@gmail.com>
2024-09-15 22:51:47 -0300

    first commit

---
+++ src/app.tsx
@@ -0,0 +1,19 @@
+import { BrowserRouter } from 'react-router-dom'
+
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+
+import { Router } from './router'
+import { GlobalStyle } from './styles/global'
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+
+      <GlobalStyle />
+    </ThemeProvider>
+  )
+}
